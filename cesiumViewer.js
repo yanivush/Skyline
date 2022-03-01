a863c1218e210e732f7b58b88d4e374da2b09440
yanivchik <yanivchik@gmail.com>
2022-03-01 14:36:20 +0200

    Update cesiumViewer.js

--- cesiumViewer.js
+++ cesiumViewer.js
@@ -92,7 +92,7 @@ function sync2DView() {
       position: Cesium.Cartesian3.fromDegrees(longitude, latitude),
       billboard: {
         image: "images/arrow.png",
-        scale : 0.5,
+        scale : 0.2,
       },
     })
   
